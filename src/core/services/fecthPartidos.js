69a3409097502a07c1f793c7c09e7ed935569021
nimpedrojo <pedro.lafuente@gmail.com>
2025-05-27 13:12:55 +0200

    user agent

--- src/core/services/fecthPartidos.js
+++ src/core/services/fecthPartidos.js
@@ -10,11 +10,14 @@ export default async function obtenerPartidos(desde, hasta) {
     const url = APIConstants.URL_FEDERACION + `&Sch_Fecha_Desde=${desde}&Sch_Fecha_Desde_input=${desdeinput}&Sch_Fecha_Hasta=${hasta}&Sch_Fecha_Hasta_input=${hastainput}`;
     const browser = await puppeteer.launch({
         executablePath: process.env.CHROME_BIN || '/app/.chrome-for-testing/chrome-linux64/chrome',
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox','--disable-blink-features=AutomationControlled','--window-size=1920,1080'],
         headless: 'new'          // recomendado desde Puppeteer v22
     });
     const page = await browser.newPage();
-
+    await page.setUserAgent(
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' +
+    '(KHTML, like Gecko) Chrome/124.0 Safari/537.36'
+    );
     await page.goto(url, { waitUntil: 'networkidle2' });
     try {
         await page.click(APIConstants.BUTTON_COOKIES); 
