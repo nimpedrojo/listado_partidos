b1af88d28c4db52d6316f7017a40e1beefecf171
nimpedrojo <pedro.lafuente@gmail.com>
2025-05-27 21:10:06 +0200

    Test if proxie solve problem

--- src/core/services/browser.js
+++ src/core/services/browser.js
@@ -4,12 +4,13 @@ import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import resolveChrome from '../utils/resolveChromePath.js';
 
 puppeteer.use(StealthPlugin());
-
+const proxy = 'http://35.236.213.248:80';
 export async function launchBrowser() {
   return puppeteer.launch({
     executablePath: resolveChrome(),   // /usr/bin/chromium en Docker
     headless: 'new',
     args: [
+      '--proxy-server=' + proxy,
       '--no-sandbox',
       '--disable-setuid-sandbox',
       '--disable-dev-shm-usage',
