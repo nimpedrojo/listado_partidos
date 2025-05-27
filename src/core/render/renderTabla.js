ffb071bfb9c82aa341f4a56ecad2b502171255a6
nimpedrojo <pedro.lafuente@gmail.com>
2025-05-27 11:14:23 +0200

    Change to front/api

--- src/core/render/renderTabla.js
+++ src/core/render/renderTabla.js
@@ -1,41 +1,13 @@
-import puppeteer from 'puppeteer';
-import {APIConstants} from '../../utils/constants.js';
+import {APIConstants} from '../utils/constants.js';
 import fs from 'fs';
-import { parsePartidos } from '../../utils/parsePartidos.js';
 import { JSDOM } from 'jsdom';
-
-
-export default async function generarListado(fechaInicio,fechaFin) {
-    const desdeinput = fechaInicio.inicio;
-    const hastainput = fechaInicio.fin;
-    const desde = desdeinput.split('-').reverse().join('-');
-    const hasta = hastainput.split('-').reverse().join('-');
-    const imagenvs = APIConstants.IMAGE_VS;
-    const URL = APIConstants.URL_FEDERACION + `&Sch_Fecha_Desde=${desde}&Sch_Fecha_Desde_input=${desdeinput}&Sch_Fecha_Hasta=${hasta}&Sch_Fecha_Hasta_input=${hastainput}`;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto(URL, { waitUntil: 'networkidle2' });
-    try {
-        await page.click(APIConstants.BUTTON_COOKIES); 
-        await page.waitForTimeout(1000);
-    } catch {
-        console.log(APIConstants.MESSAGE_NO_COOKIES);
-    }
-
-    await page.waitForSelector(APIConstants.TABLE_SELECTOR);
-
-    const html = await page.content();
-    await browser.close();
-
-    const cheerio = await import('cheerio');
-    const $ = cheerio.load(html);
-    const partidos = parsePartidos($);
-
-    const htmlTemplate = fs.readFileSync('./template.html', 'utf-8');
+/** Renderiza la tabla HTML */
+export default function renderTabla(partidos) {
+  
+    const htmlTemplate = fs.readFileSync('./src/core/render/template.html', 'utf-8');
     const dom = new JSDOM(htmlTemplate);
     const { document } = dom.window;
-
+    const imagenvs = APIConstants.IMAGE_VS;
     const contenedor = document.querySelector(APIConstants.CONTAINER_SELECTOR);
 
     const partidosPorFecha = {};
@@ -78,10 +50,5 @@ export default async function generarListado(fechaInicio,fechaFin) {
         contenedor.appendChild(partidoDiv);
         }
     }
-
-    fs.writeFileSync('./output/preview.html', dom.serialize());
-    
-    console.log("Archivo HTML generado. Ábrelo en navegador y se exportara como PDF.");
-   
-    return './output/preview.html';
+    return dom.serialize();
 }
